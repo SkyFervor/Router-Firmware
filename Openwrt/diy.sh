f05aa4fe5ab0f52e6767a784626d7832fd52216b
backup <591609998@qq.com>
2020-01-08 22:40:21 +0800

    Update diy.sh

--- Openwrt/diy.sh
+++ Openwrt/diy.sh
@@ -1,5 +1,5 @@
 echo '修改机器名称'
-sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/JDC/g' package/base-files/files/bin/config_generate
 
 #echo '修改网关地址'
 #sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
